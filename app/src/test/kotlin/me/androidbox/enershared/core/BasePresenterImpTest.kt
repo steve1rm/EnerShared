128ba0fb7a35d7060b61ea52dac5c358e7c8f9cd
= <stevekitkat@gmail.com>
2018-01-28 01:44:00 +0700

    Increased code coverage

--- app/src/test/kotlin/me/androidbox/enershared/core/BasePresenterImpTest.kt
+++ app/src/test/kotlin/me/androidbox/enershared/core/BasePresenterImpTest.kt
@@ -3,9 +3,10 @@ package me.androidbox.enershared.core
 import android.view.View
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.core.IsNull.notNullValue
-import org.junit.Assert.*
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.mock
 
 class BasePresenterImpTest {
     private lateinit var basePresenter: BasePresenter<View>
@@ -18,5 +19,14 @@ class BasePresenterImpTest {
     @Test
     fun testBasePresenterIsNotNullValue() {
         assertThat(basePresenter, `is`(notNullValue()))
+    }
+
+    @Test
+    fun testAttachViewIsViewAttachedIsTrue() {
+        val view: View = mock(View::class.java)
+
+        basePresenter.attachView(view)
+
+        assertThat(basePresenter.isViewAttached(), `is`(true))
     }
 }
