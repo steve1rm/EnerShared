66b8b06c2a01a961d73ee672b173437a67927826
= <stevekitkat@gmail.com>
2018-01-28 16:47:45 +0700

    Increased code coverage

--- app/src/test/kotlin/me/androidbox/enershared/core/BasePresenterImpTest.kt
+++ app/src/test/kotlin/me/androidbox/enershared/core/BasePresenterImpTest.kt
@@ -3,6 +3,7 @@ package me.androidbox.enershared.core
 import android.view.View
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.core.IsNull.notNullValue
+import org.hamcrest.core.IsNull.nullValue
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -28,5 +29,51 @@ class BasePresenterImpTest {
         basePresenter.attachView(view)
 
         assertThat(basePresenter.isViewAttached(), `is`(true))
+    }
+
+    @Test
+    fun testIsViewAttachedWhenViewIsNull() {
+        assertThat(basePresenter.isViewAttached(), `is`(false))
+    }
+
+    @Test
+    fun testIsViewAttachedWhenViewIsAttached() {
+        val view = mock(View::class.java)
+
+        basePresenter.attachView(view)
+        basePresenter.detachView()
+
+        assertThat(basePresenter.isViewAttached(), `is`(false))
+    }
+
+    @Test
+    fun testGetViewReturnNullIfNoViewAttached() {
+        assertThat(basePresenter.getView(), `is`(nullValue()))
+    }
+
+    @Test
+    fun testGetViewReturnView() {
+        val view = mock(View::class.java)
+
+        basePresenter.attachView(view)
+
+        assertThat(basePresenter.getView(), `is`(view))
+    }
+
+    @Test
+    fun testDetachViewWhenNoViewAttached() {
+        basePresenter.detachView()
+
+        assertThat(basePresenter.isViewAttached(), `is`(false))
+    }
+
+    @Test
+    fun testDetachViewWhenViewAttached() {
+        val view = mock(View::class.java)
+        basePresenter.attachView(view)
+
+        basePresenter.detachView()
+
+        assertThat(basePresenter.isViewAttached(), `is`(false))
     }
 }
